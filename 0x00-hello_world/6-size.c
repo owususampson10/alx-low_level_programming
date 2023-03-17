5bf30d2e1692154dc1643ac30fecd5844970e552
sampson owusu <owususampson078@gmail.com>
2023-03-16 17:42:54 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,9 +13,9 @@ int main(void)
 	float f;
 
 printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
 printf("Size of long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
-printf("Size of float: %lu bytes(s)\n", (unsigned long)sizeof(a));
+printf("Size of float: %lu bytes(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
