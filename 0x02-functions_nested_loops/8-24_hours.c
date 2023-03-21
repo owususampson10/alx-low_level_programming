dd933e7924b3055c907417aa9d0868a8d73f2e5d
sampson owusu <owususampson078@gmail.com>
2023-03-21 15:16:38 -0700

    8. I'm federal agent Jack Bauer, and today is the longest day of my life

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+
+/**
+ * jack_bauer - Prints the minutes of a day
+ *
+ * Return: no return
+ */
+	void jack_bauer(void)
+	{
+		int i, j;
+
+
+		i = 0;
+
+
+		while (i < 24)
+		{
+			j = 0;
+			while (j < 60)
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+				_putchar(':');
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+				_putchar('\n');
+				j++;
+			}
+			i++;
+		}
+	}
+
