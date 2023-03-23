98adced3020c33f7eceabe1780a0373a6f421cf4
sampson owusu <owususampson078@gmail.com>
2023-03-23 11:54:19 -0700

    largest_number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,26 +10,24 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest;
+ int largest;
 
-	if (a > b && b > c)
-	{
-		largest = a;
-	}
-	else if (a > c && c > b)
-		largest = a;
-	else if (b > a && a > c)
-	{
-		largest = b;
-	}
-	else if (b > c && c > a)
-		largest = b;
-	else if (c > a && a > b)
-		largest = c;
-	else if (c > b && b > a)
-	{
-		largest = c;
-	}
+ if (a > b && a > c)
+ {
+ largest = a;
+ }
+ else if (a > b && c > a)
+ {
+ largest = c;
+ }
+ else if (b > c)
+ {
+ largest = b;
+ }
+ else
+ {
+ largest = c;
+ }
 
-	return (largest);
+return (largest);
 }
