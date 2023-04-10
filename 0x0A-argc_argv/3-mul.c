a220bb91f868958b1359f13ef2e59455459dd055
sampson owusu <owususampson078@gmail.com>
2023-04-10 11:13:34 -0700

    3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,18 +11,18 @@
 
 int main(int argc, char *argv[])
 {
+	int x, t;
+
 	if (argc != 3)
 {
 	printf("Error\n");
 	return (1);
 }
 
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
+	x = atoi(argv[1]);
+	t = atoi(argv[2]);
 
-	int result = num1 * num2;
-
-	printf("%d\n", result);
+	printf("%d\n", x * t);
 
 	return (0);
 }
